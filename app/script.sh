d745b5e1cab9ab132d0565e1a42fc828080ce8f3
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-09 15:26:32 -0300

    versao white

--- app/script.sh
+++ app/script.sh
@@ -7,3 +7,9 @@ docker tag fabiocaettano74/web-nginx:white fabiocaettano74/web-nginx:latest
 "3. Enviar a image para o docker HUB:"
 docker push fabiocaettano74/web-nginx:white
 docker push fabiocaettano74/web-nginx:latest
+
+"4. Deletar container se ele existir:"
+docker rm -f web-nginx-white
+
+"5. Executar o container em background"
+docker run --name web-nginx-white -d -p 8080:80 fabiocaettano74/web-nginx:white
