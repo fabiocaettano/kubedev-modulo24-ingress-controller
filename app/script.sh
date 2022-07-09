a00d350f9423cb92555079b1e9147016650a6736
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-09 15:31:14 -0300

    versao green

--- app/script.sh
+++ app/script.sh
@@ -1,5 +1,11 @@
-echo "Construir o app -  Versão Green:"
+echo "1. Construir o app -  Versão Green:"
 docker build -t fabiocaettano74/web-nginx:green .
 
-echo "Enviar o app para o Docker Hub:"
+echo "2. Enviar o app para o Docker Hub:"
 docker push fabiocaettano74/web-nginx:green
+
+echo "3. Deletar container se ele existir:"
+docker rm -f web-nginx-green
+
+echo "4. Executar o container em background"
+docker run --name web-nginx-green -d -p 8080:80 fabiocaettano74/web-nginx:green
