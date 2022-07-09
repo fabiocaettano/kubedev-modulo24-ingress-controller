cd350e25dbe355927efc426cc466068f804ceb1e
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-09 15:38:15 -0300

    versao white

--- app/script.sh
+++ app/script.sh
@@ -10,6 +10,8 @@ docker push fabiocaettano74/web-nginx:latest
 
 "4. Deletar container se ele existir:"
 docker rm -f web-nginx-white
+docker rm -f web-nginx-blue
+docker rm -f web-nginx-green
 
 "5. Executar o container em background"
 docker run --name web-nginx-white -d -p 8080:80 fabiocaettano74/web-nginx:white
