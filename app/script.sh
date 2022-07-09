03726306c41897e2c9ca7ab6ddf3c29a0fbfb820
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-09 12:49:01 -0300

    versao white

--- app/script.sh
+++ app/script.sh
@@ -1,2 +1,9 @@
-docker build -t fabiocaettano74/web-nginx:green .
-docker push fabiocaettano74/web-nginx:green
+"1. Criar a image da aplicação, Versão White"
+docker build -t fabiocaettano74/web-nginx:white .
+
+"2. Criar a tag para versão latest:"
+docker tag fabiocaettano74/web-nginx:white fabiocaettano74/web-nginx:latest
+
+"3. Enviar a image para o docker HUB:"
+docker push fabiocaettano74/web-nginx:white
+docker push fabiocaettano74/web-nginx:latest
