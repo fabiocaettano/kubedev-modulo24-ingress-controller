98d58d1682938afeeb294fae5e61056dc8e28790
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-09 12:30:25 -0300

    versao blue

--- app/script.sh
+++ app/script.sh
@@ -1,2 +1,4 @@
-docker build -t fabiocaettano74/web-nginx:green .
-docker push fabiocaettano74/web-nginx:green
+echo "Construir a imagem do app"
+docker build -t fabiocaettano74/web-nginx:blue .
+echo "Realizar o push para o Docker Hub"
+docker push fabiocaettano74/web-nginx:blue
