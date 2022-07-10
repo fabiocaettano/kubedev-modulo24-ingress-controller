d211a6d370454e532e13af0c584482053e5e6b11
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-10 09:54:13 -0300

    versao green

--- app/script.sh
+++ app/script.sh
@@ -4,9 +4,9 @@ docker build -t fabiocaettano74/web-nginx:green .
 echo "3. Criar a tag para versão latest:"
 docker tag fabiocaettano74/web-nginx:green fabiocaettano74/web-nginx:latest
 
-
 echo "4. Enviar o app para o Docker Hub:"
 docker push fabiocaettano74/web-nginx:green
+docker push fabiocaettano74/web-nginx:latest
 
 echo "5. Deletar container se ele existir:"
 docker rm -f web-nginx-green
