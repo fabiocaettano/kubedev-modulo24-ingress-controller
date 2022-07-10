b190007f268dcb70f9c03f854d47041564622c9b
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-10 09:53:34 -0300

    versao blue

--- app/script.sh
+++ app/script.sh
@@ -1,10 +1,17 @@
 echo "1. Construir a imagem do app"
 docker build -t fabiocaettano74/web-nginx:blue .
-echo "2. Realizar o push para o Docker Hub"
+
+echo "3. Criar a tag para versão latest:"
+docker tag fabiocaettano74/web-nginx:blue fabiocaettano74/web-nginx:latest
+
+echo "3. Realizar o push para o Docker Hub"
 docker push fabiocaettano74/web-nginx:blue
-echo "3. Deletar container se ele existir:"
+docker push fabiocaettano74/web-nginx:latest
+
+echo "4. Deletar container se ele existir:"
 docker rm -f web-nginx-white
 docker rm -f web-nginx-green
 docker rm -f web-nginx-blue
-echo "4. Executar o container em background"
+
+echo "5. Executar o container em background"
 docker run --name web-nginx-blue -d -p 8080:80 fabiocaettano74/web-nginx:blue
