62fc5545cf9551f965b84f0deb7485c1d503eb15
fabiocaettano <fabio.caettano74@gmail.com>
2022-07-10 09:43:42 -0300

    versao white

--- app/script.sh
+++ app/script.sh
@@ -1,17 +1,17 @@
-"1. Criar a image da aplicação, Versão White"
+echo "1. Criar a image da aplicação na ersão White"
 docker build -t fabiocaettano74/web-nginx:white .
 
-"2. Criar a tag para versão latest:"
+echo "2. Criar a tag para versão latest:"
 docker tag fabiocaettano74/web-nginx:white fabiocaettano74/web-nginx:latest
 
-"3. Enviar a image para o docker HUB:"
+echo "3. Enviar a image para o Docker HUB:"
 docker push fabiocaettano74/web-nginx:white
 docker push fabiocaettano74/web-nginx:latest
 
-"4. Deletar container se ele existir:"
+echo "4. Deletar container se ele existir:"
 docker rm -f web-nginx-white
 docker rm -f web-nginx-blue
 docker rm -f web-nginx-green
 
-"5. Executar o container em background"
+echo "5. Executar o container em background"
 docker run --name web-nginx-white -d -p 8080:80 fabiocaettano74/web-nginx:white
